88cf65b67daa7829e43c5ca46eacfd104cb05cb9
Jason Whitlock <jrwhitlock7@gmail.com>
2021-03-04 18:13:44 -0700

    commit tree 18c424ecbfdc85881985f07cd72c8b40172b9a32

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
-  #your code here
+  
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key) #makes the key a reader and writer method.
+      self.send(("#{key}="), value) #.send calls the key with an arguement of value. key= makes it so the user can input their own value.
+    end
+  end
 end
+
+#we iterated over Person.new{name: "Sophie", user_name: "sm_debenedetto", age: 26, location: "NY, NY", bio: "I'm a programmer living in NY!"}
